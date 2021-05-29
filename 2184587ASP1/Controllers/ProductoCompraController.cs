2232e0176935bd0296f68a4e6b1d724cfbac4f8c
Daniel Martinez <daniel.santiago2002@hotmail.es>
2021-05-29 02:38:15 -0500

    Crud completo productocompra con llave foranea

---
+++ 2184587ASP1/Controllers/ProductoCompraController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2184587ASP1.Models
+{
+    public class ProductoCompraController : Controller
+    {
+        // GET: ProductoCompra
+        public ActionResult Index()
+        {
+            using (var db = new inventarioEntities())
+            {
+                return View(db.producto_compra.ToList());
+            }
+        }
+
+        // GET: ProductoCompra/Details/5
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventarioEntities())
+            {
+                var findProduct = db.producto_compra.Find(id);
+                return View(findProduct);
+            }
+        }
+
+        public ActionResult listarProductos()
+        {
+            using (var db = new inventarioEntities())
+            {
+                return PartialView(db.producto.ToList());
+            }
+        }
+
+        public ActionResult listarCompras()
+        {
+            using (var db = new inventarioEntities())
+            {
+                return PartialView(db.compra.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(producto_compra producto_Compra)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    db.producto_compra.Add(producto_Compra);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    producto_compra findProductBuy = db.producto_compra.Where(a => a.id == id).FirstOrDefault();
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(producto_compra editProducto_compra)
+        {
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    producto_compra producto_compra = db.producto_compra.Find(editProducto_compra.id);
+                    producto_compra.id_compra = editProducto_compra.id_compra;
+                    producto_compra.id_producto = editProducto_compra.id_producto;
+                    producto_compra.cantidad = editProducto_compra.cantidad;
+                    
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    var findProductoCompra = db.producto_compra.Find(id);
+                    db.producto_compra.Remove(findProductoCompra);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+    }
+}
