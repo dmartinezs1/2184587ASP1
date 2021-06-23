c97fb0c37866808e61e7411cfb183c1a06512352
Daniel Martinez <daniel.santiago2002@hotmail.es>
2021-06-23 11:44:13 -0500

    cargue csv tabla clientes

--- 2184587ASP1/Controllers/clientesController.cs
+++ 2184587ASP1/Controllers/clientesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -56,6 +57,61 @@ namespace _2184587ASP1.Controllers
             }
 
             return View(cliente);
+        }
+
+        public ActionResult uploadCSV()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult uploadCSV(HttpPostedFileBase fileForm)
+        {
+            //string para guardar la ruta
+            string filePath = string.Empty;
+
+            //condicion para saber si llego o no el archivo
+            if (fileForm != null)
+            {
+                //ruta de la carpeta que caragara el archivo
+                string path = Server.MapPath("~/Uploads/");
+
+                //verificar si la ruta de la carpeta existe
+                if (!System.IO.Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                //obtener el nombre del archivo
+                filePath = path + Path.GetFileName(fileForm.FileName);
+                //obtener la extension del archivo
+                string extension = Path.GetExtension(fileForm.FileName);
+
+                //guardando el archivo
+                fileForm.SaveAs(filePath);
+
+                string csvData = System.IO.File.ReadAllText(filePath);
+                foreach (string row in csvData.Split('\n'))
+                {
+                    if (!string.IsNullOrEmpty(row))
+                    {
+                        var newCliente = new cliente
+                        {
+                            nombre = row.Split(';')[0],
+                            documento = row.Split(';')[1],
+                            email = row.Split(';')[2],
+                        };
+
+                        using (var db = new inventarioEntities())
+                        {
+                            db.cliente.Add(newCliente);
+                            db.SaveChanges();
+                        }
+                    }
+                }
+
+            }
+            return View("");
         }
 
         // GET: clientes/Edit/5
